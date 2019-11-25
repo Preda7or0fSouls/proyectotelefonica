12fd8d981ef0e8f2b1f51278b4cace31bbb1fb6a
sllop <sllop@sllop-VirtualBox>
2019-11-25 20:07:27 +0100

    version 1:"Esqueleto proyecto"

---
+++ src/modelo/Modelo.java
@@ -0,0 +1,20 @@
+package modelo;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Modelo
+ */
+@Stateless
+@LocalBean
+public class Modelo {
+
+    /**
+     * Default constructor. 
+     */
+    public Modelo() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
